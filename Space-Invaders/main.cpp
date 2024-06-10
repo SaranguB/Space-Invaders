0926c9af0b1373715fc4343bd6dc2d0739c84e11
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-06-10 15:58:47 +0530

    Drawing Shapes

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -2,9 +2,11 @@
 int main()
 {
 
-	sf::VideoMode video(1280, 270);
+	sf::VideoMode video(800, 600);
 	sf::RenderWindow window(video, "SFML window");
 	window.setFramerateLimit(60);
+
+	int spaceY = 200;
 
 	while (window.isOpen())
 	{
@@ -17,7 +19,25 @@ int main()
 				window.close();
 		}
 
-		window.clear(sf::Color::Blue);
+		window.clear(sf::Color::White);
+
+		sf::CircleShape circle(50);
+		circle.setFillColor(sf::Color::Green);
+		circle.setPosition(50, spaceY);
+
+		sf::CircleShape square(50, 4);
+		square.setFillColor(sf::Color::Red);
+		square.setPosition(300, spaceY);
+
+		sf::CircleShape triangle(50, 3);
+		triangle.setFillColor(sf::Color::Blue);
+		triangle.setPosition(550, spaceY);
+
+		window.draw(circle);
+		window.draw(square);
+		window.draw(triangle);
+
+
 		window.display();
 
 
