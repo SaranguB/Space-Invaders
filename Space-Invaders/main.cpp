7c30949166cebf3893a585755732fcd0e4cf8ec6
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-06-11 11:50:15 +0530

    Starting Off with the player

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,62 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+
+class Player
+{
+private:
+
+	int playerHealth = 3;
+	int playerScore = 0;
+	sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
+
+
+public:
+
+	int moveSpeed = 5;
+	sf::Texture playerTexture;
+	sf::Sprite playerSprite;
+
+	int GetScore()
+	{
+		return playerScore;
+	}
+	void SetScore(int newScore)
+	{
+		playerScore += newScore;
+	}
+
+
+	void TakeDamage();
+	void Move();
+	void ShootBullets();
+
+};
 
 int main()
 {
-    return 0;
+	sf::VideoMode videoMod(800, 600);
+	sf::RenderWindow window(videoMod, "space invader");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		Player player;
+
+		window.clear(sf::Color::Blue);
+
+		window.display();
+	}
+
+
+	return 0;
 }
