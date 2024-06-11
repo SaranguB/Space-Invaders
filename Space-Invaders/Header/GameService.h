2678d5bf01606d5a58a555a085656a0638d83473
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-06-11 15:30:59 +0530

    Game Loop

---
+++ Space-Invaders/Header/GameService.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class GameService
+{
+private:
+	void initialize();
+	void destroy();
+public:
+	GameService();
+    ~GameService();
+
+	void ignite();
+	void update();
+	void render();
+	bool isRunning();
+
+
+
+
+
+
+};
---
+++ Space-Invaders/Source/GameService.cpp
@@ -0,0 +1,45 @@
+#include "../Header/GameService.h";
+
+void GameService::initialize()
+{
+	//to initialize
+}
+
+void GameService::destroy()
+{
+	//clean up resources
+}
+
+GameService::GameService()
+{
+	//constructor
+}
+
+GameService::~GameService()
+{
+	//destructor
+}
+void GameService::ignite()
+{
+	//starts the game
+}
+
+void GameService::update()
+{
+	// Updates the game logic and game state.
+}
+
+void GameService::render()
+{
+}
+
+bool GameService::isRunning()
+{
+	// Checks if the game is currently running.
+}
+
+
+int main()
+{
+
+}
