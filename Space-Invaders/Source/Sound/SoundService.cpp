8230a47f67a1450a63fa177584237f0e02b31840
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-06-27 10:23:43 +0530

    Sound Service

---
+++ Space-Invaders/Source/Sound/SoundService.cpp
@@ -0,0 +1,25 @@
+#include "../../Header/Sound/SoundService.h"
+
+namespace Sound
+{
+	
+	void SoundService::Initialize()
+	{
+	}
+
+	void SoundService::PlayBackgroundMusic()
+	{
+	}
+
+	void SoundService::PlaySound(SoundType soundType)
+	{
+	}
+
+	void SoundService::LoadBackgroundMusicFromFile()
+	{
+	}
+
+	void SoundService::LoadSoundFromFile()
+	{
+	}
+}
