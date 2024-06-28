8c50b04520d9a27146794429c91b019d947b0913
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-06-28 14:18:26 +0530

    Icollectible

---
+++ Space-Invaders/Header/Collectible/Collectible.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <SFML/System/Vector2.hpp>
+
+namespace Collectible
+{
+	class ICollectible
+	{
+	public:
+		virtual ~ICollectible() = 0;
+		
+		virtual void OnCollected() = 0;
+
+		virtual void Initialize(sf::Vector2f position) = 0;
+		virtual void Update() = 0;
+		virtual void Render() = 0;
+
+		virtual sf::Vector2f GetCollectiblePosition() = 0;
+
+	};
+
+	inline ICollectible::~ICollectible(){}
+}
