2219f19b6d77c806c25cea34d00d04e237b8cdcb
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-06-28 09:49:41 +0530

    Service

--- Space-Invaders/Source/Bullet/BulletService.cpp
+++ Space-Invaders/Source/Bullet/BulletService.cpp
@@ -1,14 +1,76 @@
 #include "../../Header/Bullet/BulletService.h"
+#include "../../Header/Bullet/BulletController.h"
+#include "../../Header/Bullet/BulletConfig.h"
+#include "../../Header/Bullet/Controllers/FrostBulletController .h"
+#include "../../Header/Bullet/Controllers/LaserBulletController .h"
+#include "../../Header/Bullet/Controllers/TorpedoeController.h"
 
 namespace Bullet
 {
+	using namespace Controller;
+	using namespace Projectile;
+	BulletService::BulletService()
+	{
+	}
+	BulletService::~BulletService()
+	{
+		Destroy();
+	}
+
+	BulletController* BulletService::SpawnBullet(BulletType bulletType, sf::Vector2f position, MovementDirection direction)
+	{
+		BulletController* bulletController = CreateBullet(bulletType);
+
+		bulletController->Initialize(position, direction);
+		bulletList.push_back(bulletController);
+		return bulletController;
+	}
+
+	void BulletService::DestroyBullet(BulletController* bulletController)
+	{
+		bulletList.erase(std::remove(bulletList.begin(), bulletList.end(), bulletController), bulletList.end());
+		delete(bulletController);
+	}
+
+	BulletController* BulletService::CreateBullet(BulletType bulletType)
+	{
+
+		switch (bulletType)
+		{
+		case BulletType::LASER_BULLET:
+			return new LaserBulletController(Bullet::BulletType::LASER_BULLET);
+
+		case BulletType::FROST_BULLET:
+			return new FrostBulletController(Bullet::BulletType::FROST_BULLET);
+		case BulletType::TORPEDO:
+			return new FrostBulletController(Bullet::BulletType::TORPEDO);
+		}
+
+	}
+
+	void BulletService::Destroy()
+	{
+		for (int i = 0;i < bulletList.size();i++)
+		{
+			delete(bulletList[i]);
+		}
+	}
 	void BulletService::Initialize()
 	{
 	}
 	void BulletService::Update()
 	{
+		for (int i = 0;i < bulletList.size();i++)
+		{
+			bulletList[i]->Update();
+		}
 	}
 	void BulletService::Render()
 	{
+		for (int i = 0;i < bulletList.size();i++)
+		{
+			bulletList[i]->Render();
+		}
 	}
+
 }
