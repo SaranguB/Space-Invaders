af4bc8479f90a5392642821a369b6e25d41ebbb9
SaranguB <163407247+SaranguB@users.noreply.github.com>
2024-07-01 12:03:39 +0530

    Revert "d"
    This reverts commit 071f0bbe4c713c0470225dacf9d32259384c818f.

--- Space-Invaders/Header/UI/UIService.h
+++ Space-Invaders/Header/UI/UIService.h
@@ -1,42 +1,32 @@
 #pragma once
 #include "../../Header/UI/Main Menu/MainMenuUIController.h"
 #include "../../Header/UI/Interface/IUIcontroller.h"
-#include <SFML/Graphics.hpp>
+
 
 namespace UI
 {
-	namespace UI
+
+	class UIService 
 	{
-		enum class UIState
-		{
-			VISIBLE,
-			HIDDEN,
-		};
+	private:
 
-		class UIView
-		{
-		protected:
+		MainMenu::MainMenuUIController* mainMenuUIController;
 
-			MainMenu::MainMenuUIController* mainMenuUIController;
+		void CreateControllers();
+		void InitializeController();
+		void Destroy();
+		Interface::IUIcontroller* GetCurrentUIController();
 
-			sf::RenderWindow* gameWindow;
-			UIState uiState;
+	public:
 
-		
-			
-			Interface::IUIcontroller* GetCurrentUIController();
+		UIService();
+		~UIService();
 
-		public:
-
-			UIService();
-			virtual ~UIService();
-
-			void Initialize();
-			void Update();
-			void Render();
-			void ShowScreen();
+		void Initialize();
+		void Update();
+		void Render();
+		void ShowScreen();
 
 
-		};
-	}
+	};
 }
